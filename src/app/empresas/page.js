395c31700ef63a82bd6ca64521613823cd2a495a
cubo1991 <adavidlopezmathez@gmail.com>
2025-09-20 10:22:05 -0300

    prueba con mock

--- src/app/empresas/page.js
+++ src/app/empresas/page.js
@@ -1,26 +1,23 @@
 'use client';
 import { useEffect, useState } from "react";
-import { getEmpresasSafe } from "../../services/Empresas/EmpresasService";
 import CompanyCard from "../components/CompanyCard";
-
-
-
-
-
+import companies from "../constants/dummyCompanies";
 
 export default function EmpresasPage() {
-  const [companies, setCompanies] = useState([]);
+  const [mockCompanies, setCompanies] = useState([]);
 
   useEffect(() => {
-    getEmpresasSafe(setCompanies);
+    setCompanies(companies.empresas);
+
   }, []);
+
 
   return (
     <main className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Empresas del Club BA</h1>
+      <h1 className="text-2xl font-bold mb-4">Empresas del Club BIA</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-        {companies.map((company, index) => (
-          <CompanyCard key={index} {...company} />
+        {mockCompanies.map((company) => (
+          <CompanyCard key={company.id} {...company} />
         ))}
       </div>
     </main>
